b27c8a4ef4631fea4cc218e70d8d986b83facce1
Abhighogare <104622937+Abhighogare@users.noreply.github.com>
2026-01-05 12:08:55 +0530

    Added Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: "voting-app",
+      script: "server.js",
+
+      // ✅ NEW WORKING DIRECTORY (FIX)
+      cwd: "/home/azureuser/apps/voting-app",
+
+      exec_mode: "fork",
+      instances: 1,
+
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000
+      }
+    }
+  ]
+};
